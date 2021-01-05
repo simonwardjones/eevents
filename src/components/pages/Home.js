9c9fd9e2b8824c0d2168c161ddf7400e843cc5be
simonwardjones <simon.ward-jones@carwow.co.uk>
2021-01-05 20:53:00 +0000

    remove un-used import

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,10 @@
-import React from "react";
-
 const Home = () => {
-  return (
-    <div>
-      <button>Create New Event</button>
-      <button>Use Code To Find Event</button>
-    </div>
-  );
+    return (
+        <div>
+            <button>Create New Event</button>
+            <button>Use Code To Find Event</button>
+        </div>
+    );
 };
 
 export default Home;
