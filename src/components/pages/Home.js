6fa30112ba3e27826582462d4081afd00868895e
robMolloy <robmolloy@hotmail.co.uk>
2021-01-05 19:58:18 +0000

    home page + 2 buttons

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <button>Create New Event</button>
+      <button>Use Code To Find Event</button>
+    </div>
+  );
+};
+
+export default Home;
