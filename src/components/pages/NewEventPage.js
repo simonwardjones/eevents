4f7e8a999d9561a560e521075564e7f91fe5a930
simonwardjones <simon.ward-jones@carwow.co.uk>
2021-01-07 19:54:19 +0000

    Added custom useless logo

--- src/components/pages/NewEventPage.js
+++ src/components/pages/NewEventPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import logo from "../../logo.svg";
 
 const NewEventPage = () => {
-    return <div>New Event Page</div>;
+    return (
+        <div>
+            New Event Page
+            <img src={logo} className="App-logo" alt="logo" />
+        </div>
+    );
 };
 
 export default NewEventPage;
