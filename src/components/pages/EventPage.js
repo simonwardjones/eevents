1a42f3997e0c49a3e27c56582168537a7948a565
robMolloy <robmolloy@hotmail.co.uk>
2021-01-07 21:36:26 +0000

    Updates

--- src/components/pages/EventPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const EventPage = () => {
-    return <div> Example Event Page</div>;
-};
-
-export default EventPage;
--- src/components/pages/NewEventPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NewEventPage = () => {
-    return <div>New Event Page</div>;
-};
-
-export default NewEventPage;
