db04fffe8d787652eab97f3636801cd24f9a3265
robMolloy <robmolloy@hotmail.co.uk>
2021-01-10 21:19:44 +0000

    Link not required

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import Home from "./components/pages/Home";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import EventPage from "./components/pages/EventPage";
 import NewEventPage from "./components/pages/NewEventPage";
+import Navbar from "./components/custom/navigation/Navbar";
 
 function App() {
     const Index = Home;
