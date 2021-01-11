c37495c676a8ccf95a6e903910bbc968692afb0d
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 15:48:31 +0000

    Builds on Text component instead of standalone

--- src/components/generic/text/Title.js
+++ src/components/generic/text/Title.js
@@ -1,32 +1,55 @@
 import React from "react";
+
+import Text from "./Text";
+
 import { makeStyles } from "@material-ui/core/styles";
 
+import React from "react";
+
 const Title = (props = {}) => {
-  let children, size, className, align, marginBottom, centerXs;
-  ({
-    children,
-    size = 0,
-    className = "",
-    align = "left",
-    marginBottom = 0,
-    centerXs = true,
-    ...props
-  } = props);
+    let children, size;
+    ({ children, size } = props);
 
-  let ComponentOptions = ["h1", "h2", "h4"];
-  let Component = ComponentOptions[size];
+    let Component = ["h1", "h2", "h4"][size];
 
-  const classes = makeStyles((theme) => ({
-    root: { textAlign: align, marginBottom: theme.spacing(marginBottom) },
-  }))();
-
-  const centerXsClass = centerXs ? "text-center-xs" : "";
-
-  return (
-    <Component className={`${className} ${classes.root} ${centerXsClass}`}>
-      {children}
-    </Component>
-  );
+    return (
+        <Text Component={component} {...props}>
+            {children}
+        </Text>
+    );
 };
 
 export default Title;
+
+/*
+
+const Title = (props = {}) => {
+    let children, size, className, align, marginBottom, centerXs;
+    ({
+        children,
+        size = 0,
+        className = "",
+        align = "left",
+        marginBottom = 0,
+        centerXs = true,
+        ...props
+    } = props);
+
+    let ComponentOptions = ["h1", "h2", "h4"];
+    let Component = ComponentOptions[size];
+
+    const classes = makeStyles((theme) => ({
+        root: { textAlign: align, marginBottom: theme.spacing(marginBottom) },
+    }))();
+
+    const mobileXsClasses = centerXs ? "text-center-xs" : "";
+
+    return (
+        <Component className={`${className} ${classes.root} ${centerXsClass}`}>
+            {children}
+        </Component>
+    );
+};
+
+export default Title;
+*/
