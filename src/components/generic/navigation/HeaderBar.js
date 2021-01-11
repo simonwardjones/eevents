3d5b1fbc3a22da2ac8bc3453658b636b153e66f4
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 22:43:19 +0000

    HeaderBar style changes

--- src/components/generic/navigation/HeaderBar.js
+++ src/components/generic/navigation/HeaderBar.js
@@ -6,52 +6,58 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Container from "../../generic/containers/Container";
 
-const useStyles = makeStyles((theme) => ({
-  root: {},
-  appbar: {
-    textAlign: "center",
-    backgroundColor: "#FFFFFF",
-    position: "relative",
-  },
+// const headerHeight
 
-  menuButton: {
-    color: theme.palette.grey.main,
-    border: `solid 1px ${theme.palette.grey.main}`,
-    borderRadius: "4px",
-    padding: "4px 8px",
-    position: "absolute",
-    top: "50%",
-    right: "0",
-    transform: "translate(0%, -50%)",
-  },
-  logo: { maxHeight: "150px", maxWidth: "65%", padding: "30px 0" },
+const useStyles = makeStyles((theme) => ({
+    root: {},
+    appbar: {
+        textAlign: "left",
+        backgroundColor: "#FFFFFF",
+        position: "relative",
+    },
+    container: {
+        // position: "relative",
+        display: "flex",
+        justifyContent: "space-between",
+        alignContent: "center",
+    },
+    menuButton: {
+        alignSelf: "center",
+        color: theme.palette.grey.main,
+        border: `solid 1px ${theme.palette.grey.main}`,
+        borderRadius: "4px",
+        padding: "4px 8px",
+    },
+    logo: {
+        maxHeight: "100px",
+        maxWidth: "65%",
+    },
 }));
 
 const HeaderBar = (props = {}) => {
-  let toggleDrawer, imgSrc;
-  ({ toggleDrawer, imgSrc, ...props } = props);
-  const classes = useStyles();
+    let toggleDrawer, imgSrc;
+    ({ toggleDrawer, imgSrc, ...props } = props);
+    const classes = useStyles();
 
-  return (
-    <AppBar className={classes.appbar}>
-      <MUIToolbar className={classes.toolbar}>
-        <Container style={{ position: "relative" }}>
-          <img src={imgSrc} className={classes.logo} alt="" />
+    return (
+        <AppBar className={classes.appbar}>
+            <MUIToolbar className={classes.toolbar} variant="dense">
+                <Container className={classes.container}>
+                    <img src={imgSrc} className={classes.logo} alt="" />
+                    <IconButton
+                        onClick={toggleDrawer}
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="menu"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </Container>
+            </MUIToolbar>
+        </AppBar>
 
-          <IconButton
-            onClick={toggleDrawer}
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-        </Container>
-      </MUIToolbar>
-    </AppBar>
-
-    // </div>
-  );
+        // </div>
+    );
 };
 
 export default HeaderBar;
