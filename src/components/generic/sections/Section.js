b98f4fb08690b960a7a2c3badb8ae58de565e122
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 22:45:04 +0000

    Section - default padding removed and prop to add container as parent of children

--- src/components/generic/sections/Section.js
+++ src/components/generic/sections/Section.js
@@ -5,11 +5,17 @@ import { makeStyles } from "@material-ui/core/styles";
 import Container from "../containers/Container";
 
 const useStyles = makeStyles((theme) => {
-    return { root: { padding: theme.spacing(2) } };
+    return {
+        root: {
+            // padding: theme.spacing(2),
+        },
+    };
 });
 
 const Section = (props = {}) => {
-    let children, className, withContainer;
+    let children,
+        className = "",
+        withContainer;
     ({ children, className, withContainer = false, ...props } = props);
 
     const Component = withContainer ? Container : React.Fragment;
