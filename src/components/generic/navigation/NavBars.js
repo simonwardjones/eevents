a489085a9ab0775736f062365cece767c96a3a1b
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 15:45:27 +0000

    Add space after NavBars

--- src/components/generic/navigation/NavBars.js
+++ src/components/generic/navigation/NavBars.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SideBar from "../../generic/navigation/SideBar";
 import HeaderBar from "../../generic/navigation/HeaderBar";
+import Br from "../../generic/text/Br";
 
 const NavBars = (props = {}) => {
     let drawerOpen, imgSrc;
@@ -14,6 +15,7 @@ const NavBars = (props = {}) => {
             <HeaderBar imgSrc={imgSrc} toggleDrawer={toggleDrawer} />
 
             <SideBar state={drawerState} toggleDrawer={toggleDrawer} />
+            <Br />
         </>
     );
 };
