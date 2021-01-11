cd1efab4aea259827ff94a61adc5df926cbfda40
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 15:42:30 +0000

    NavBars syntax

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Home from "./components/pages/Home";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navbar from "./components/generic/navigation/NavBars";
+import NavBars from "./components/generic/navigation/NavBars";
 import AllRoutes from "./components/generic/navigation/AllRoutes";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     return (
         <Router basename={`${process.env.REACT_APP_PUBLIC_PATH}`}>
             <div className="App">
-                <Navbar imgSrc="logo.svg" />
+                <NavBars imgSrc="logo.svg" />
                 <main>
                     <Route exact path="/" component={Index} />
                     <AllRoutes />
