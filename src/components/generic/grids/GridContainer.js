56072bb94d91049b0584864a61749edf19024482
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 15:45:08 +0000

    SpacingY implementation changed

--- src/components/generic/grids/GridContainer.js
+++ src/components/generic/grids/GridContainer.js
@@ -2,34 +2,36 @@ import React from "react";
 import MUIGrid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => {
-  return {
-    root: {
-      // maxWidth: "100%",
-      // margin: `${theme.spacing(5)}px auto`,
-    },
-  };
-});
-
 const GridContainer = (props = {}) => {
-  let children, className, spacing, spacingY;
-  ({ children, className = "", spacing = 5, spacingY = 1, ...props } = props);
+    let children, className, spacing, spacingY;
+    ({
+        children,
+        className = "",
+        spacing = 5,
+        spacingY = spacing,
+        ...props
+    } = props);
 
-  const classes = useStyles();
+    const classes = makeStyles((theme) => ({
+        root: {},
+        spacingY: {
+            "& div.MuiGrid-item": {
+                paddingTop: theme.spacing(spacingY / 2),
+                paddingBottom: theme.spacing(spacingY / 2),
+            },
+        },
+    }))();
 
-  let spacingYOptions = ["spacing-y-0", ""];
-  let spacingYClassName = spacingYOptions[spacingY];
-
-  return (
-    <MUIGrid
-      container
-      className={`${className} ${classes.root} ${spacingYClassName}`}
-      spacing={spacing}
-      {...props}
-    >
-      {children}
-    </MUIGrid>
-  );
+    return (
+        <MUIGrid
+            container
+            className={`${className} ${classes.root} ${classes.spacingY}`}
+            spacing={spacing}
+            {...props}
+        >
+            {children}
+        </MUIGrid>
+    );
 };
 
 export default GridContainer;
