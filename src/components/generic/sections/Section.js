cb1150ce4815fc1f183b7fe13c599c6041da1f10
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 15:46:23 +0000

    prop option to have container in section - false by default

--- src/components/generic/sections/Section.js
+++ src/components/generic/sections/Section.js
@@ -2,24 +2,25 @@ import React from "react";
 
 import MUIBox from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
+import Container from "../containers/Container";
 
 const useStyles = makeStyles((theme) => {
-  return {
-    box: { padding: theme.spacing(2) },
-  };
+    return { root: { padding: theme.spacing(2) } };
 });
 
 const Section = (props = {}) => {
-  let children, className;
-  ({ children, className, ...props } = props);
+    let children, className, withContainer;
+    ({ children, className, withContainer = false, ...props } = props);
 
-  const classes = useStyles();
+    const Component = withContainer ? Container : React.Fragment;
 
-  return (
-    <MUIBox className={`${className} ${classes.box}`} {...props}>
-      {children}
-    </MUIBox>
-  );
+    const classes = useStyles();
+
+    return (
+        <MUIBox className={`${className} ${classes.root}`} {...props}>
+            <Component>{children}</Component>
+        </MUIBox>
+    );
 };
 
 export default Section;
