af58275a187a75b3f36e88690ce54152a6fea902
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 15:49:54 +0000

    Style added - get default props added

--- src/components/pages/NewEventPage.js
+++ src/components/pages/NewEventPage.js
@@ -1,19 +1,23 @@
 import React from "react";
-import Form from "../generic/forms/Form";
-import Input from "../generic/fields/Input";
+
+import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import "yup-phone";
-import { yupResolver } from "@hookform/resolvers/yup";
+
+import Container from "../generic/containers/Container";
+import Form from "../generic/forms/Form";
+import Input from "../generic/fields/Input";
 import GridItem from "../generic/grids/GridItem";
 import GridContainer from "../generic/grids/GridContainer";
+import Section from "../generic/sections/Section";
 
 const Schema = yup.object().shape({
     event_name: yup
         .string()
+        .required("Shtick it in my box")
         .min(3, "needs to be longer, Silad! :(")
-        .max(10, "I can't handle something that big, Silad!")
-        .required("Shtick it in my box"),
+        .max(10, "I can't handle something that big, Silad!"),
 });
 
 // const Schema = yup.object().shape({
@@ -27,42 +31,56 @@ const Schema = yup.object().shape({
 const NewEventPage = () => {
     const form = useForm({ resolver: yupResolver(Schema), mode: "onChange" });
 
+    const getDefaultFieldProps = ({ form, name }) => {
+        return {
+            name,
+            ref: form.register,
+            helperText: form.errors?.[name]?.message,
+            error: !!form.errors[name],
+        };
+    };
+
     return (
-        <Form>
-            <GridContainer>
-                <GridItem xs={12} sm={6}>
-                    <Input
-                        name="event_name"
-                        ref={form.register}
-                        helperText={form.errors?.event_name?.message}
-                        error={!!form.errors.event_name}
-                        label="Event Name"
-                    />
-                </GridItem>
-                <GridItem xs={12} sm={6}>
-                    <Input name="host_name" label="Host Name" />
-                </GridItem>
-                <GridItem xs={12} sm={6}>
-                    <Input name="description" label="Description" />
-                </GridItem>
-                <GridItem xs={12} sm={6}>
-                    <Input name="location" label="Location" />
-                </GridItem>
-                <GridItem xs={12} sm={6}>
-                    <Input name="guests_qty" label="No. Of Guests" />
-                </GridItem>
-                <GridItem xs={12} sm={6}>
-                    <Input name="year" label="Year" />
-                </GridItem>
-                <GridItem xs={12} sm={6}>
-                    <Input name="month" label="Month" />,
-                    <Input name="day" label="Day" />
-                </GridItem>
-                <GridItem xs={12} sm={6}>
-                    <Input name="contact_number" label="Contact Number" />
-                </GridItem>
-            </GridContainer>
-        </Form>
+        <Section withContainer>
+            <Form>
+                <GridContainer>
+                    <GridItem xs={12} sm={6}>
+                        <Input
+                            {...getDefaultFieldProps({
+                                form,
+                                name: "event_name",
+                            })}
+                            label="Event Name"
+                        />
+                    </GridItem>
+                    <GridItem xs={12} sm={6}>
+                        <Input name="host_name" label="Host Name" />
+                    </GridItem>
+                    <GridItem xs={12} sm={6}>
+                        <Input name="description" label="Description" />
+                    </GridItem>
+                    <GridItem xs={12} sm={6}>
+                        <Input name="location" label="Location" />
+                    </GridItem>
+                    <GridItem xs={12} sm={4}>
+                        <Input name="day" label="Day" />
+                    </GridItem>
+                    <GridItem xs={12} sm={4}>
+                        <Input name="month" label="Month" />
+                    </GridItem>
+                    <GridItem xs={12} sm={4}>
+                        <Input name="year" label="Year" />
+                    </GridItem>
+                    <GridItem xs={12} sm={6}>
+                        <Input name="guests_qty" label="No. Of Guests" />
+                    </GridItem>
+
+                    <GridItem xs={12} sm={6}>
+                        <Input name="contact_number" label="Contact Number" />
+                    </GridItem>
+                </GridContainer>
+            </Form>
+        </Section>
     );
 };
 
