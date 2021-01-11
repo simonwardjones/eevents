fa245bcc8f0448d0624af4155f0e58239b787845
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 15:49:19 +0000

    Stylised

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,20 @@
+import ButtonPrimary from "../generic/buttons/ButtonPrimary";
+import Container from "../generic/containers/Container";
+import Section from "../generic/sections/Section";
+import ButtonRow from "../generic/grids/ButtonRow";
+
 const Home = () => {
     return (
-        <div>
-            <button>Create New Event</button>
-            <button>Use Code To Find Event</button>
-        </div>
+        <Section>
+            <Container>
+                <ButtonRow
+                    array={[
+                        <ButtonPrimary>Create New Event</ButtonPrimary>,
+                        <ButtonPrimary>Use Code To Find Event</ButtonPrimary>,
+                    ]}
+                />
+            </Container>
+        </Section>
     );
 };
 
