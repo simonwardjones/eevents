79263a1fd7280edee52fdf29de4c07bc7afbf348
robMolloy <robmolloy@hotmail.co.uk>
2021-01-11 15:44:24 +0000

    helperText turned off as default

--- src/components/generic/fields/Input.js
+++ src/components/generic/fields/Input.js
@@ -3,31 +3,31 @@ import { TextField } from "@material-ui/core/";
 import makeStyles from "@material-ui/styles/makeStyles";
 
 const Input = React.forwardRef((props = {}, ref) => {
-  let helperText;
-  ({ helperText = " ", ...props } = props);
+    let helperText;
+    ({ helperText = "", ...props } = props);
 
-  const classes = makeStyles((theme) => ({
-    root: {},
-    input: {
-      backgroundColor: `${theme.palette.grey.light}`,
-      "&:focus": { backgroundColor: `${theme.palette.grey[50]}` },
-    },
-    label: { zIndex: "1", color: theme.palette.grey.dark },
-  }))();
+    const classes = makeStyles((theme) => ({
+        root: {},
+        input: {
+            backgroundColor: `${theme.palette.grey.light}`,
+            "&:focus": { backgroundColor: `${theme.palette.grey[50]}` },
+        },
+        label: { zIndex: "1", color: theme.palette.grey.dark },
+    }))();
 
-  return (
-    <TextField
-      className={classes.root}
-      fullWidth
-      size="medium"
-      inputRef={ref}
-      inputProps={{ className: classes.input }}
-      InputLabelProps={{ className: classes.label }}
-      variant="filled"
-      helperText={helperText}
-      {...props}
-    />
-  );
+    return (
+        <TextField
+            className={classes.root}
+            fullWidth
+            size="medium"
+            inputRef={ref}
+            inputProps={{ className: classes.input }}
+            InputLabelProps={{ className: classes.label }}
+            variant="filled"
+            helperText={helperText}
+            {...props}
+        />
+    );
 });
 
 export default Input;
